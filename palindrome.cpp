921be2d80346c9f6dc634c5404e6391da778f7c1
Nidhi Sahu27 <149890086+NidhiSahu2@users.noreply.github.com>
2024-09-21 16:49:21 +0530

    Add files via upload

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,19 +1,21 @@
-class Solution{
-public:	
-	
-	
-	int isPalindrome(string s)
-	{
-	    // Your code goes here//
-	
-	 if(s.length()<=1)
-	 return 1;
-	 else if (s[0]!= s[s.length()-1])
-	    return 0;
-	   else
-	   return isPalindrome(s.substr(1,s.length()-2));
-	   
-	    
-	}
+class Solution {
+public:
+    bool isPalindrome( int x) {
+        if(x<0)
+        {
+            return false;
+        }
 
+        long  long reverse =0;
+        long long temp=x;
+        while(temp!=0)
+        {
+     int digit = temp%10;
+     reversed = reversed = reversed *10+digit;
+     temp/=10;
+
+
+        }
+        return (reversed ==x);
+    }
 };
